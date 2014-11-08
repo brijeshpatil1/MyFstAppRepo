9e3467fe9f57ae5e60f60ddf031befbf9842d82b
brijeshpatil1 <brijeshpatil1989@gmail.com>
2014-11-08 21:57:37 +0530

    Uploaded Project Files

---
+++ WebForm1.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+            ShowHeaderWhenEmpty="True" DataKeyNames="stateid" 
+            onrowcancelingedit="GridView1_RowCancelingEdit" 
+            onrowcommand="GridView1_RowCommand" onrowdeleting="GridView1_RowDeleting" 
+            onrowediting="GridView1_RowEditing" onrowupdating="GridView1_RowUpdating">
+            <Columns>
+                <asp:TemplateField HeaderText="StateID">
+                    <EditItemTemplate>
+                        <asp:Label ID="Label2" runat="server" Text='<%# bind("stateid") %>'></asp:Label>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label1" runat="server" Text='<%# bind("stateid") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="StateName">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox1" runat="server" Text='<%# bind("statename") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <HeaderTemplate>
+                        <asp:TextBox ID="TextBox2" runat="server" PlaceHolder="Enter StateName"></asp:TextBox>
+                        &nbsp;
+                        <asp:Button ID="Button1" runat="server" Text="Add" CommandName="Add" />
+                    </HeaderTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label3" runat="server" Text='<%# bind("statename") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:CommandField ShowEditButton="True" />
+                <asp:CommandField ShowDeleteButton="True" />
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+        SqlDataAdapter da;
+        DataTable dt;
+        SqlCommand cmd;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                FillGrid();
+            }
+        }
+
+        private void FillGrid()
+        {
+            da = new SqlDataAdapter("select * from state", con);
+            dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Add")
+            {
+                cmd = new SqlCommand("insert into state values(@sname)", con);
+                cmd.Parameters.AddWithValue("@sname", ((TextBox)GridView1.HeaderRow.FindControl("TextBox2")).Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                FillGrid();
+            }
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            FillGrid();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            FillGrid();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            cmd = new SqlCommand("update state set statename=@sname where stateid=@SID",con);
+            cmd.Parameters.AddWithValue("@sname",((TextBox)GridView1.Rows[e.RowIndex].FindControl("TextBox1")).Text);
+            cmd.Parameters.AddWithValue("@sid", Convert.ToInt16(GridView1.DataKeys[e.RowIndex].Value));
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            GridView1.EditIndex = -1;
+            FillGrid();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            cmd = new SqlCommand("delete from state where stateid=@SID", con);
+            cmd.Parameters.AddWithValue("@sid", Convert.ToInt16(GridView1.DataKeys[e.RowIndex].Value));
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            FillGrid();
+        }
+    }
+}
